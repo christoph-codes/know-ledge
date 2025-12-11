cde2a428840de3c0d10aa9adefef62ad37b8ebc6
christoph-codes <tkcwebdev@gmail.com>
2025-12-11 13:51:16 -0800

    main: Initialize typescript

---
+++ backend/bin/index.js
@@ -0,0 +1,18 @@
+// ESM
+import Fastify from "fastify";
+const fastify = Fastify({
+    logger: true,
+});
+// Declare a route
+fastify.get("/", function (request, reply) {
+    console.log("request", request);
+    reply.send({ hello: "world" });
+});
+// Run the server!
+fastify.listen({ port: Number(process.env.PORT) || 5555 }, function (err, address) {
+    if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+    fastify.log.info(`server listening on ${address}`);
+});
--- backend/index.ts
+++ backend/index.ts
@@ -7,14 +7,18 @@ const fastify = Fastify({
 
 // Declare a route
 fastify.get("/", function (request, reply) {
+	console.log("request", request);
 	reply.send({ hello: "world" });
 });
 
 // Run the server!
-fastify.listen({ port: Number(process.env.PORT) || 3000 }, function (err) {
-	if (err) {
-		fastify.log.error(err);
-		process.exit(1);
+fastify.listen(
+	{ port: Number(process.env.PORT) || 5555 },
+	function (err, address) {
+		if (err) {
+			fastify.log.error(err);
+			process.exit(1);
+		}
+		fastify.log.info(`server listening on ${address}`);
 	}
-	// Server is now listening on ${address}
-});
+);
