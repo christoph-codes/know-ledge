0123bd45c3d4e46ac5ec4947f302f4685afdb816
christoph-codes <tkcwebdev@gmail.com>
2025-12-11 15:35:36 -0800

    main: Initialize shadcn with button component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
 	return (
 		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
 			<main className="flex h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
 				<h1>Hey</h1>
+				<Button>Click Me</Button>
 			</main>
 		</div>
 	);
