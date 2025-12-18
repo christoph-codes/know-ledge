0bfd6fdfe00518bc5cb7aef5e78bb2cdd5895560
christoph-codes <tkcwebdev@gmail.com>
2025-12-18 13:16:40 -0800

    5: Setup supabase database and connect with all endpoints

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,8 @@
 // ESM
+import "dotenv/config";
 import Fastify from "fastify";
 import healthCheckRoutes from "./routes/healthcheck.routes.js";
+import supabase from "plugins/supabase";
 
 const fastify = Fastify({
 	logger: true,
@@ -8,11 +10,10 @@ const fastify = Fastify({
 
 const host = "RENDER" in process.env ? `0.0.0.0` : `localhost`;
 const port = Number(process.env.PORT) || 5555;
-console.log("port", port);
 
 // Register routes
+fastify.register(supabase);
 fastify.register(healthCheckRoutes);
-// fastify.register(supabase);
 
 // Run the server!
 fastify.listen({ host, port }, function (err, address) {
--- backend/routes/healthcheck.routes.ts
+++ backend/routes/healthcheck.routes.ts
@@ -4,9 +4,10 @@ export default async function healthCheckRoutes(fastify: FastifyInstance) {
 	fastify.get(
 		"/healthcheck",
 		async (request: FastifyRequest, reply: FastifyReply) => {
-			console.log("Healthcheck requested", request.body);
+			console.log("Healthcheck requested");
 			return reply.send({
 				status: "ok",
+				isDBConnected: Boolean(fastify.supabase),
 				timestamp: new Date(),
 			});
 		}
